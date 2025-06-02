dbd634df702b4133e18fe34d508ce4a4dba9539a
Jaba-Klava <kristinasun55@gmail.com>
2025-06-02 21:10:12 +0300

    Delete RxJava directory

--- RxJava/src/main/java/com/rx_hw/Scheduler.java
+++
@@ -1,9 +0,0 @@
-package com.rx_hw;
-
-/**
- * Интерфейс планировщика задач для асинхронного выполнения
- */
-public interface Scheduler {
-    /** Выполнить задачу в соответствующем потоке/пуле */
-    void execute(Runnable task);
-}
